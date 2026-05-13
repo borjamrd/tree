29efa58332f4ff7f89cb31713d683d9ce82258e0
Borja <borja.munoz@feverup.com>
2026-05-13 22:43:22 +0200

    chore: eslint and prettier

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  trailingComma: 'es5',
+}
+
+export default config
